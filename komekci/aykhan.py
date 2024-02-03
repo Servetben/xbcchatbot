7d73dde83744d22f8aa0d4f0c6f6eb7e0770b9ce
Abbasxan <138109623+Abbasxan@users.noreply.github.com>
2024-02-03 22:03:13 +0400

    Add files via upload

---
+++ komekci/aykhan.py
@@ -0,0 +1,21 @@
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
+
+
+from telethon import TelegramClient
+
+# Config məlumatları
+
+# Telegram Client (Telethon)
+API_ID = "12210813"
+API_HASH = "e42eeae11a2f96bcfc5ec3b46a30adad"
+bot_token = "5884794395:AAEXjrDOBYlt0YrOjmzLtl8VlwWWz7Hk_oA"
+
+# Nermin
+Nermin = TelegramClient('Nermin', API_ID, API_HASH).start(bot_token=bot_token)
