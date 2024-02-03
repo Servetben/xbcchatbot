6c39afd68d2d429f79df503076fe82081a382b69
Abbasxan <138109623+Abbasxan@users.noreply.github.com>
2024-02-03 22:00:55 +0400

    Create mesaj.py

---
+++ mesajlar/mesaj.py
@@ -0,0 +1,290 @@
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
+
+salam = (
+"Salam",
+"Salam Kişi",
+"Salam Ay Xanım",
+"Salam Balam",
+"Salamdaa",
+"Uşş balama salamm",
+"Salam Cənab 🫶",
+"Salam Nəəfəəəss",
+)
+
+necesen = (
+"Həmşəki Kimi! Sən?",
+"Saol",
+"Həkimsən ?",
+"Ə belədana 😂",
+"What",
+"İyyim aşkım sen ?",
+"yaxşı olmağa çalışıram",
+"Mən başımı buraxe sən necəsən 😂",
+)
+
+
+
+sagol = (
+"Salam Sağol",
+"Hara gedsən",
+"Yatıram demə🥲",
+"Sağolunnn yenə gözləyəriyy🙈",
+"Uşş balam Sağol",
+"Sağol canım benim 🫶",
+"Sağol Kişi 🔥",
+"Sağo Ay Xanım",
+)
+
+
+
+getdim = (
+"Hara?",
+"Cəhənəmə get",
+"Get Heç Gəlmə",
+"Neynim? Getdə 😒",  
+"Yaxşı Yol", 
+"Dalıncada Su Atımı?",
+)
+
+geldim = (
+"Xoş Gəldin ❤️",
+"Xoş Gəldin Nəəəfəəssss",  
+"Neynim?",  
+"Qırmızı Xalçada Sərəkmi?",  
+)
+
+sesizKOLGE = (
+"Haycannn Aşkım ❤",
+"Haycannn, Quzu kəsime sənə 🙊",
+"Bəliii 🫶",
+"Nə gözəl deyirsəne, birdə de",
+"O Mənim Ürəyimdi ❤",
+"Na Var Aaa, Nə İsdiyirsən? 😑",
+"Azzzaarrr 😬😬",
+"Buyur 😝",
+"🙄",  
+"🥱",  
+"❤", 
+"Na Var Aaa, Nəyə Tağ Edirsənn?? 🙄",  
+"Aəəəə Dincəəldəəəə 😒",  
+"😒",  
+"Qrupun Gözüüü 😝",  
+"Adəə İmkan Ver Yataqdaa 🥱🥱",  
+"Əlini Farahat Qoyda Alaa 😒",  
+"Dur Cıx Çölə",  
+"what? 🙄", 
+"Davaaay Nömrənii Att Kişi Balaa 👨‍💻", 
+"Sən Öl Səni Yolacam aaaa",
+)
+
+ban = (
+"Vəhşii",
+"Həri Vəhşii",
+"Vəhşi Panteramm kimə ban atdın",
+"Havada ban kokusu var",
+)
+
+emoji1 = (
+"Na Var A Yuxarda? Nəyə Baxırsan? 🙄",
+"Ba Ba Gözlərin Gənə Qalxızdl Boş Kəlləsinə 😒",
+"Gözlərin Çixmasın Yazıqsan , Nəyə Baxırsan? 😶",
+)
+
+emoji2 = (
+"Na Var Aa ? Nəyə Gülürsən? 🙄 ",  
+"Az Gül Yekə Qizsan 🥱", 
+"Hırıldama 😑",
+"Xonça Baş Az gül 😝", 
+"Quzu Kəsərəm O Gülüşünə 😄",
+"Gülüşə Baaa Elə Bil Dağ Mışıdı 😅", 
+) 
+
+fed = (
+"XAOS ❤ Di ",  
+"Yaşasın XAOS ❤",  
+"Salam Olsun XAOS Ailəsinə 👋", 
+"XAOS Ailəsindən Hamıya Salam ❤",  
+"Buyur Nə Lazımdı? 🤔", 
+)
+  
+niye = (
+"Maraqlı Olsun deyə 🥱",
+"Elə Elə 🪓",
+"Azzar Niyəə 😜",
+"Niyəsidə Var Əəə Xonça Baş 😂",
+"Uşşş 😍😍",
+"De Sən Ölll 🙄", 
+"Davay Fly ✈",
+"Saçlarını Yolanda Biləsən 🥹",
+)
+  
+  
+ne = (
+"Nənəə 😂",
+"Azzarr Nəə 🙄",
+"Deyirəm Salam 🤦",
+"Elə Eləə 😐",
+"Qinamiram Eyy , Qandığın Qırafa Odu 😂",
+"Deyirəmm Ki Lavaşş Var? 🤦",
+"Haycann 🥲",
+"Ağzına Pişmiş Gənə 🤣",
+"Elə ondan 🪓",
+"Deyirəm Evlisən? 😊",
+)  
+  
+  
+hay = (
+"Nəəəəə ? 🙄",
+"Azzaarr Hayyy 🤦",
+"Qaraazaaarr 😂",
+"Deyrəm Nətərsən? 🙄",
+"98 İn Çeşqası Var Bez Krasqa? 🤦",
+"Bəlkə 50 Cə Qəpik Pulun Ola? 😂",
+)  
+  
+  
+mal = (
+"What? 🙄",
+"Sənsən 😝",
+"Məndə Chat Bot 🥲",
+"Şad Oldum 😊",
+"Səndən Öyrənmişəmdə 🥹",
+"Haycann Aşkımm 😍",
+"Hə Balamm ☺️",
+"Mal mənə Deyenin Qaynenesidi Bildin Əəə??? ",
+)  
+  
+  
+can = (
+"Azzzaarr 🥹",
+"Canın Baaal 😍",
+"Haycan Aşkım 🥺",
+"Nə Gözəl Deyirsən 🥲 Birdə Dedə 🥹",
+"Haycan Balam 😍",
+"Nə Var Aaa?? 🙄",
+"Tay Az Qizdara Pqd Xod Elə Əə.🪓",
+"Canın Cənnət Necəsən? 🙄",
+"Dur Çıx Çölə Gözüm Səni Görməsin 😁",
+"What ? 🙄",
+"Dediyini Başa Düşmədim Nə Deyrsən əəə?? 😡",
+)  
+  
+  
+balam = (
+"Canın Bal Əzizim 😊",
+"Azzzaar 🤦",
+"Yavaş əəəə 😳",
+"What? 😳",
+"Canın Bal ☺️",
+"Yolacam a səni bu dəqiqə 🙄",
+"Necəsən ürəyim? 🥱",
+"Aaa 🥹 bidə dedə 🥹",
+"Döyülməyin gəlub aa sənin deyən 🥺",
+)  
+  
+  
+xos = (
+"Heç xoş gəlmədin 😑",
+"Xoş gəlmisən amma ged 👿", "Mənləsən əə xötəyyy?",
+"Xoş kimdi qadan alım 🥱",
+"Bizim oralarda xoş gələni döyurlər🥴",
+"Xoş gününə aşkım ❤️",
+"Haycan balam 🥲",
+"Rədd oll 🙄😒",
+)
+
+hara = (
+"Yapış divara 🤤",
+"Hara desən ora 😬",
+"Hara lazımdı ?",
+"Yanıma 🫣",
+"Sene ne hara?",
+"Cəhənnəməə",
+"Zibilliyəəə",
+"Hara Lazımdı ora ay maall",
+)
+
+gel = (
+"Gəlimdə balam 🙊",
+"Gəlsəm pis olar 😋",
+"Gəlirəm ha 😈",
+"Hara gəlim balam? 🤡",
+"Strelka at qaqaş 💪",
+)
+
+
+
+gordum = (
+"Elə bildim korsan 😒",
+"Görüb nağarassan xötey? 😡",
+"Haramı 🙈",
+"Görmədim özüm gostərdim 😉",
+"He məndə gördüm 😄",
+"Qələt Eədin 😁",
+"Neynim? 🤦",
+)
+  
+  
+taım = (
+"https://t.me/addtheme/sf158WSw7LWOtpvV",
+"https://t.me/addtheme/bpcrFtP4qYu0DdnJ",
+"https://t.me/addtheme/aUFKCX7AQ3aQpDjp",
+"https://t.me/addtheme/L7HVQjC4UUyOfL9y",
+"https://t.me/addtheme/Qd4eBWTIOH4Ai3Zv",
+"https://t.me/addtheme/NightWolf",
+"https://t.me/addtheme/GreenBlack",
+"https://t.me/addtheme/TvldPzYmpG8LqkY3",
+"https://t.me/addtheme/Q4GuvNPpMvG59G6V",
+"https://t.me/addtheme/kGQaW0HHsjc7oFOv",
+"https://t.me/addtheme/z3E6vkceX0pfmo5P",
+"https://t.me/addtheme/poMW3amfnwUwOefI",
+"https://t.me/addtheme/l1felAbEVNQCN3NW",
+"https://t.me/addtheme/y6xMaSuBOmuGekHj",
+"https://t.me/addtheme/Fp6h6JpzXrWnjF9y",
+"https://t.me/addtheme/Purple_Grapes",
+"https://t.me/addtheme/xQNP1Jp2aklmldNx",
+"https://t.me/addtheme/ry0AgHsISs439fxi",
+"https://t.me/addtheme/ZHl93FYO9ja7hN81",
+"https://t.me/addtheme/gc2MlPyKHMBjw5WS",
+"https://t.me/addtheme/ciNZt5N6QCFjsrQI",
+"https://t.me/addtheme/bEKOF0v8XuLAFZ6P",
+"https://t.me/addtheme/IOSTelegramThemes2020_11july",
+"https://t.me/addtheme/DarkPink_1",
+"https://t.me/addtheme/Halloween_04",
+"https://t.me/addtheme/BlackBlue_ByYamila",
+"https://t.me/addtheme/NewYorkNyVK",
+"https://t.me/addtheme/blackcircles_ByYamila",
+"https://t.me/addtheme/KINGByVK",
+"https://t.me/addtheme/MRPERFECTBYVK",
+"https://t.me/addtheme/ChanchiNeonByVK",
+"https://t.me/addtheme/SamurayByVK",
+"https://t.me/addtheme/NeonRocks_ByYamila",
+"https://t.me/addtheme/StichOhanaByVK",
+"https://t.me/addtheme/SkullDarkByVK",
+"https://t.me/addtheme/RedGirlByVK",
+"https://t.me/addtheme/SpidermanByVK",
+"https://t.me/addtheme/CuteMelodyByVK",
+"https://t.me/addtheme/YouAreBeautifulStichByVK",
+"https://t.me/addtheme/ManchesterUnitedByVK",
+)  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
