c2b2d6badec10b9eb77a6197663ccc5245acc47e
Abbasxan <138109623+Abbasxan@users.noreply.github.com>
2024-02-04 23:27:43 +0400

    Update bot.py

--- mesajlar/bot.py
+++ mesajlar/bot.py
@@ -1,12 +1,3 @@
-# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
-# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
-# Öz adına çıxaran peysərdi
-# Bu yazıları silmədən işlədin
-
-# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
-# t.me/aykhan_s | insta: aykhan026 | 
-# GitHub: aykhan026
-
 yeni_user = (
 "Xoş Gəldin Balam 😍",
 "Səni gözləməydən gözüm qaralıbe gəl çıxdaa",
